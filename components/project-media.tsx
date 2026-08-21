572a891b89882620034322f3c12bfe5ca8434ff5
Saad Mughal <saadmughal321@gmail.com>
2026-08-22 02:33:39 +0500

    feat(ui): add editorial dark overlay and animated corner brackets hover effect

--- components/project-media.tsx
+++ components/project-media.tsx
@@ -78,6 +78,24 @@ export function ProjectMedia({
           className={cn("object-cover", imageClassName)}
         />
       )}
+
+      {/* Editorial Hover Overlay & Inset Corner Brackets */}
+      <div
+        aria-hidden="true"
+        className="pointer-events-none absolute inset-0 z-10 bg-black/20 opacity-0 transition-opacity duration-300 ease-[cubic-bezier(0.25,1,0.5,1)] group-hover:opacity-100"
+      >
+        {/* Top-Left Corner Bracket */}
+        <span className="absolute top-2.5 left-2.5 sm:top-3.5 sm:left-3.5 h-3 w-3 sm:h-3.5 sm:w-3.5 border-t-[1.5px] border-l-[1.5px] border-white -translate-x-1 -translate-y-1 opacity-0 transition-all duration-300 ease-[cubic-bezier(0.25,1,0.5,1)] group-hover:translate-x-0 group-hover:translate-y-0 group-hover:opacity-100" />
+
+        {/* Top-Right Corner Bracket */}
+        <span className="absolute top-2.5 right-2.5 sm:top-3.5 sm:right-3.5 h-3 w-3 sm:h-3.5 sm:w-3.5 border-t-[1.5px] border-r-[1.5px] border-white translate-x-1 -translate-y-1 opacity-0 transition-all duration-300 ease-[cubic-bezier(0.25,1,0.5,1)] group-hover:translate-x-0 group-hover:translate-y-0 group-hover:opacity-100" />
+
+        {/* Bottom-Left Corner Bracket */}
+        <span className="absolute bottom-2.5 left-2.5 sm:bottom-3.5 sm:left-3.5 h-3 w-3 sm:h-3.5 sm:w-3.5 border-b-[1.5px] border-l-[1.5px] border-white -translate-x-1 translate-y-1 opacity-0 transition-all duration-300 ease-[cubic-bezier(0.25,1,0.5,1)] group-hover:translate-x-0 group-hover:translate-y-0 group-hover:opacity-100" />
+
+        {/* Bottom-Right Corner Bracket */}
+        <span className="absolute bottom-2.5 right-2.5 sm:bottom-3.5 sm:right-3.5 h-3 w-3 sm:h-3.5 sm:w-3.5 border-b-[1.5px] border-r-[1.5px] border-white translate-x-1 translate-y-1 opacity-0 transition-all duration-300 ease-[cubic-bezier(0.25,1,0.5,1)] group-hover:translate-x-0 group-hover:translate-y-0 group-hover:opacity-100" />
+      </div>
     </div>
   );
 }
